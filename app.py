b2b3243eac0159c4289eb6a69f516c7bfce4a85b
faisalchattha34-dot <faisalchattha34@gmail.com>
2025-11-02 02:12:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from collections import deque, Counter
 st.set_page_config(layout="wide", page_title="ASL Detection - YOLOv8")
 st.title("🤖 American Sign Language Detection (YOLOv8)")
 
-MODEL_PATH = "models/best.pt"   # ✅ fixed lowercase
+MODEL_PATH = "best.pt"   # ✅ fixed lowercase
 
 # Cache YOLO model for faster reload
 @st.cache_resource
